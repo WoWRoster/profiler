6d9055b3852acf0f936ba1369c1e8a2f4622863b
Joshua <c.treyce@gmail.com>
2010-11-23 08:33:53 +0000

    WrProfiler: - Changed description for WrGP - Code formatting

--- wowrostergp/localization.deDE.lua
+++ wowrostergp/localization.deDE.lua
@@ -5,5 +5,5 @@ end
 local addon = select(2, ...)
 addon.L = setmetatable({
 	["save"] = "speichern",
-  ["Click to export your Guild Profile"] = "Klicken, um Ihr Profil Guild Export",
+	["Click to export your Guild Profile"] = "Klicken, um Ihr Profil Guild Export",
 }, {__index = addon.L})
--- wowrostergp/localization.esES.lua
+++ wowrostergp/localization.esES.lua
@@ -5,5 +5,5 @@ end
 local addon = select(2, ...)
 addon.L = setmetatable({
 	["save"] = "grabar",
-  ["Click to export your Guild Profile"] = "Pulsa aqui, para exportar la lista de tu hermandad",
+	["Click to export your Guild Profile"] = "Pulsa aqui, para exportar la lista de tu hermandad",
 }, {__index = addon.L})
